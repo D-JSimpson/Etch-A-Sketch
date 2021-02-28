ec61f64e83625c1d509e5fe6f425c8216797b10f
dante <simpsondante03@gmail.com>
2021-02-28 14:01:53 -0500

    Allow the user to enter desired grid sized after clearing

--- script.js
+++ script.js
@@ -1,17 +1,22 @@
 const container = document.getElementById('container');
 const clearButton = document.getElementById('clear');
+console.log(container.clientWidth);
 
-clearButton.addEventListener('click', clear);
 
-makeGrid();
 
-function makeGrid(){
-for(let i = 0; i <= 255; i++){
+makeGrid(16);
+
+function makeGrid(size){
+    let divWidth = (container.clientWidth)/size;
+    let newSize = size * size;
+    console.log(size);
+    container.style.cssText = "grid-template-columns: repeat(" + size + ", " + divWidth + "px); grid-template-rows: repeat(" + size + ", " + divWidth + "px);"
+for(let i = 0; i <= newSize - 1; i++){
     const div = document.createElement('div');
     div.className = 'grid-square';
-    div.style.cssText = "background-color: red; border: 1px solid black; box-sizing: border-box;"
+    div.style.cssText = "background-color: black; "
     container.appendChild(div);
-    div.addEventListener('mouseenter', function(){div.style.backgroundColor = 'blue';});
+    div.addEventListener('mouseenter', function(){div.style.backgroundColor = 'white';});
     }
 }
 
@@ -19,3 +24,13 @@ function clear(){
     let gridSelector = document.querySelectorAll('.grid-square');
     gridSelector.forEach(function(currentValue, currentIndex, listObj){gridSelector[currentIndex].remove()});
 }
+
+clearButton.addEventListener('click', function(){
+    clear();
+    let gridSize = window.prompt('What would you like th new grid size to be?');;
+    while(gridSize % 1 !== 0 || gridSize > 100){
+    alert("Enter a NUMBER that is also less than or equal to 100");
+    gridSize = window.prompt('What would you like th new grid size to be?');
+    }
+    makeGrid(gridSize);
+});
