550619801f05079b633cde9135365134d225277a
dante <simpsondante03@gmail.com>
2021-02-28 13:15:37 -0500

    Add mouseenter event that changes the background color of the divs and a clear button

--- script.js
+++ script.js
@@ -1,10 +1,21 @@
 const container = document.getElementById('container');
-container.style.cssText = "display: grid; grid-template-columns: repeat(16, 50px); grid-template-rows: repeat(16, 50px);"
+const clearButton = document.getElementById('clear');
 
+clearButton.addEventListener('click', clear);
+
+makeGrid();
+
+function makeGrid(){
 for(let i = 0; i <= 255; i++){
     const div = document.createElement('div');
     div.className = 'grid-square';
-    div.innerText = "Hello";
     div.style.cssText = "background-color: red; border: 1px solid black; box-sizing: border-box;"
     container.appendChild(div);
+    div.addEventListener('mouseenter', function(){div.style.backgroundColor = 'blue';});
+    }
+}
+
+function clear(){
+    let gridSelector = document.querySelectorAll('.grid-square');
+    gridSelector.forEach(function(currentValue, currentIndex, listObj){gridSelector[currentIndex].remove()});
 }
