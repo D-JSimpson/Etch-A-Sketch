b50c005282b7fab608414806b8ccfb3fe61c898e
dante <simpsondante03@gmail.com>
2021-03-03 13:45:42 -0500

    Create a funciton that changes the color of the sqaures by a percentage increment that is able to be turned on and off

--- script.js
+++ script.js
@@ -1,8 +1,10 @@
 const container = document.getElementById('container');
 const clearButton = document.getElementById('clear');
+const percentButton = document.getElementById('percent-mode');
+const percentOffButton = document.getElementById('percent-mode-off');
 
-let marker = 'white';
-let background = 'black';
+let marker = 'rgb(255,255,255)';
+let background = 'rgb(0,0,0)';
 
 function markerChange(){
     marker = document.getElementById('marker-dropdown').value;
@@ -19,18 +21,56 @@ function backgroundChange(){
 let drawToggle = false;
 let percentMode = false;
 
+percentButton.addEventListener('click', function(){percentMode = true;});
+percentOffButton.addEventListener('click', function(){percentMode = false;});
 container.addEventListener('click', function(){drawToggle = true;});
 container.addEventListener('dblclick', function(){drawToggle = false;});
 
 makeGrid(16);
 
 function draw(div){
-    if(drawToggle === true){
-        if(marker == 'neon green'){div.style.backgroundColor = '#00FF80'}
-        else if(marker == 'hot pink'){div.style.backgroundColor = '#FF0080'}
+    if(drawToggle === true && percentMode === false){
+        if(marker == 'neon green'){div.style.backgroundColor = '#00FF80';}
+        else if(marker == 'hot pink'){div.style.backgroundColor = '#FF0080';}
         else{
             div.style.backgroundColor = marker;
         }
+    }
+}
+
+let p = 0.1;
+
+function percentDraw(div){
+    if(drawToggle === true && percentMode === true){
+       
+       let backcolor = div.style.backgroundColor;
+       if(backcolor === 'rgb(255, 255, 255)'){
+        div.style.backgroundColor = 'rgb(0,0,0, 0.1)';
+       }
+       else{
+       let opacityloc = backcolor.indexOf('0.');
+       let parenthesis = backcolor.indexOf(')');
+       let opacity = backcolor.substring(opacityloc, parenthesis);
+       backcolor = backcolor.substring(0, opacityloc);
+       console.log(opacity);
+       console.log(backcolor);
+       opacity = +opacity + 0.1;
+       backcolor += (opacity + ')');
+       console.log(opacity);
+       console.log(backcolor);
+       div.style.backgroundColor = backcolor;
+       }
+
+        /*
+        let bc = div.style.backgroundColor = 'rgb(0, 0, 0, ' + p + ')';
+        let int = bc.indexOf('0.');
+        let int2 = bc.indexOf(')');
+        let part = bc.substring(int, int2);
+        console.log(bc);
+        console.log(int);
+        console.log(part);
+        p+= 0.1;
+        */
     }
 }
 
@@ -42,9 +82,10 @@ function makeGrid(size){
 for(let i = 0; i <= newSize - 1; i++){
     const div = document.createElement('div');
     div.className = 'grid-square';
-    div.style.cssText = "background-color: " + background +";"
+    div.style.cssText = "background-color: " + background +";";
     container.appendChild(div);
     div.addEventListener('mousemove', function(){draw(this)});
+    div.addEventListener('mouseenter', function(){percentDraw(this)});
     }
 }
 
